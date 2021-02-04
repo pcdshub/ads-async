fa6dcfc347e0dd94f83c073d2fcd1aee97ffe8aa
Ken Lauer <klauer@slac.stanford.edu>
2021-02-04 08:55:08 -0800

    MNT: move test code over to ads-log-daemon

--- ads_async/asyncio/client.py
+++ ads_async/asyncio/client.py
@@ -1,7 +1,6 @@
 import asyncio
 import collections
 import ctypes
-import json
 import typing
 from typing import Optional
 
@@ -327,7 +326,7 @@ class AsyncioClient:
             server_host: typing.Tuple[str, int],
             server_net_id: Optional[str] = None,
             client_net_id: Optional[str] = None,  # can be determined later
-            reconnect_rate : Optional[int] = 10,
+            reconnect_rate: Optional[int] = 10,
             ):
         if server_net_id is None:
             server_net_id = f'{server_host[0]}.1.1'
@@ -727,79 +726,3 @@ class AsyncioClient:
             _, port = source
             await self.send(self.client.remove_notification(handle),
                             port=port)
-
-
-def to_logstash(header: structs.AoEHeader,
-                message: structs.AdsNotificationLogMessage) -> dict:
-    custom_json = {
-        "port_name": message.sender_name.decode('ascii'),
-        "ams_port": message.ams_port,
-        "source": repr(header.source),
-        "identifier": message.unknown,
-    }
-
-    # From:
-    # AdsNotificationLogMessage(timestamp=datetime.datetime,
-    #                           unknown=84, ams_port=500,
-    #                           sender_name=b'TCNC',
-    #                           message_length=114, message=b'\'Axis
-    #                           1\' (Axis-ID: 1): The axis needs the
-    #                           "Feed Forward Permission" for forward
-    #                           positioning (error-code: 0x4358) !')
-    # To:
-    # (f'{"schema":"twincat-event-0","ts":{twincat_now},"plc":"LogTest",'
-    #   '"severity":4,"id":0,'
-    #   '"event_class":"C0FFEEC0-FFEE-COFF-EECO-FFEEC0FFEEC0",'
-    #   '"msg":"Critical (Log system test.)",'
-    #   '"source":"pcds_logstash.testing.fbLogger/Debug",'
-    #   '"event_type":3,"json":"{}"}'
-    #   ),
-    return {
-        "schema": "twincat-event-0",
-        "ts": message.timestamp.timestamp(),
-        "severity": 0,  # hmm
-        "id": 0,  # hmm
-        "event_class": "C0FFEEC0-FFEE-COFF-EECO-FFEEC0FFEEC0",
-        "msg": message.message.decode('latin-1'),
-        "source": "logging.aggregator/Translator",
-        "event_type": 0,  # hmm
-        "json": json.dumps(custom_json),
-    }
-
-
-if __name__ == '__main__':
-    async def test():
-        async with AsyncioClient(
-            server_host=('localhost', 48898),
-            server_net_id='172.21.148.227.1.1',
-            client_net_id='172.21.148.164.1.1'
-        ) as client:
-            device_info = await client.get_device_information()
-            client.log.info('Device info: %s', device_info)
-            project_name = await client.get_project_name()
-            client.log.info('Project name: %s', project_name)
-            app_name = await client.get_app_name()
-            client.log.info('Application name: %s', app_name)
-            task_names = await client.get_task_names()
-            client.log.info('Task names: %s', task_names)
-
-            # Give some time for initial notifications, and prune any stale
-            # ones from previous sessions:
-            await asyncio.sleep(1.0)
-            await client.prune_unknown_notifications()
-            async for header, _, sample in client.enable_log_system():
-                try:
-                    message = sample.as_log_message()
-                except Exception:
-                    client.log.exception('Got a bad log message sample? %s',
-                                         sample)
-                    continue
-
-                client.log.info("Log message %s ==> %s", message,
-                                to_logstash(header, message))
-
-    from .. import log
-    log.configure(level='DEBUG')
-    # log.configure(level='INFO')
-
-    value = asyncio.run(test(), debug=True)
